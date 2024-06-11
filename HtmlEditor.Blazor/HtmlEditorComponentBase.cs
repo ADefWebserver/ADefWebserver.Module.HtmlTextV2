b5042aad62418a0be2a7523993b8f2d3ff475e2a
Cody <cody@decacon.com>
2024-06-10 17:44:03 -0700

    Update HtmlEditorComponentBase.cs

--- HtmlEditor.Blazor/HtmlEditorComponentBase.cs
+++ HtmlEditor.Blazor/HtmlEditorComponentBase.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
 using Microsoft.AspNetCore.Components;
-using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
 namespace HtmlEditor

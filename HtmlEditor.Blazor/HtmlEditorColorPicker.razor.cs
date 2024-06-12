97fccce2a642519b3a1971f797e32713fc989ada
Cody <cody@decacon.com>
2024-06-12 14:12:59 -0700

    remove unused namespaces

--- HtmlEditor.Blazor/HtmlEditorColorPicker.razor.cs
+++ HtmlEditor.Blazor/HtmlEditorColorPicker.razor.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using HtmlEditor.Blazor.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace HtmlEditor.Blazor
 {
